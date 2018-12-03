05d8376f6c9f4caded375489357b44b6b349bb26
wangying <728206179@qq.com>
2018-12-03 09:23:18 +0800

    门店后台

--- dao/shopDao.js
+++ dao/shopDao.js
@@ -27,7 +27,6 @@ module.exports.addShops = async (addShops) => {
     }
 }
 module.exports.getShops = async ({ curPage, eachPage}) => {
-    console.log(1)
     let result =
         {
             curPage: Number(curPage),
@@ -38,9 +37,29 @@ module.exports.getShops = async ({ curPage, eachPage}) => {
     result.maxPage = Math.ceil(result.total / result.eachPage);
     result.rows = await seatsModel
         .find()
-        // .populate(["theaterId"])//关联的ID        
+        .populate(["usersId"])//关联的ID        
         .skip((result.curPage - 1) * result.eachPage)//最大页数
         .limit(result.eachPage)//每一页显示条数 
         .sort({ _id: 1 })//排序
     return result;
+}
+module.exports.getById = async (data) => {
+    return await mongoose.model("shop").find(data)
+}
+//修改数据
+module.exports.updataShops = async (msg) => {
+    let {_id}=msg
+    let data = await mongoose.model("shop").updateOne({ _id }, { $set: msg })
+    if (data) {
+        return true;
+    }
+}
+module.exports.postById = async (msg) => {
+//    console.log(msg)
+   let { _id,shopEmployee}=msg
+//    console.log(_id)
+   console.log()
+    let data = await mongoose.model("shop").updateOne( {_id},{'$push':{ shopEmployee :JSON.parse(shopEmployee)}})
+    
+   console.log(data)
 }
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { addShops,getShops} = require("../dao/shopDao.js")
+const { addShops,getShops,getById,updataShops,postById} = require("../dao/shopDao.js")
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -10,5 +10,14 @@ router.post('/addShops', async function (req, res, next) {
 });
 router.get('/getShops', async function (req, res, next) {
   res.send(await getShops(req.query));
+});
+router.get('/getById', async function (req, res, next) {
+  res.send(await getById(req.query));
+});
+router.post('/updataShops', async function (req, res, next) {
+  res.send(await updataShops(req.body));
+});
+router.post('/postById', async function (req, res, next) {
+  res.send(await postById(req.body));
 });
 module.exports = router;
